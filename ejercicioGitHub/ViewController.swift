0d25733361e175c06d465791c1ab4181f8d60a13
oscar20 <oscar_almazan@outlok.com>
2018-05-01 20:13:07 -0500

    Initial Commit

---
+++ ejercicioGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ejercicioGitHub
+//
+//  Created by Oscar Almazan Lora on 01/05/18.
+//  Copyright © 2018 iOS Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
