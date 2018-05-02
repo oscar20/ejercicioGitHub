d520e1df70d1ab355ef7e9300c25f16ce6cf06bd
oscar20 <oscar_almazan@outlok.com>
2018-05-01 20:24:02 -0500

    primer commit en app delegate y tercero en viewcontroller

--- ejercicioGitHub/AppDelegate.swift
+++ ejercicioGitHub/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //This is my first commit on appdelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
